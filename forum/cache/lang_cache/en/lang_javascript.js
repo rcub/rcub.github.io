c90494fc9ac129c31cc49410c88895700072e621
Julio Guerra <julio@farjump.io>
2016-09-15 09:40:21 +0200

    New mirror using
    	wget --mirror --convert-links --adjust-extension --page-requisites \
               --no-parent --exclude-directories=forum2,dld,photos,admin,forum/lofiversion,lofiversion \
    	   --reject '*calendar*,*threaded*,index.php?act=calendar*' -p http://www.rcub.info/

---
+++ forum/cache/lang_cache/en/lang_javascript.js
@@ -0,0 +1,64 @@
+/**
+* JS Language File: Français
+*/
+
+var message_pop_up_lang =
+{
+	// DEFAULT
+	'error'			   : 'Une erreur a &eacute;t&eacute; rencontr&eacute;e',
+	'no_permission'	   : 'Vous n&#039;avez pas l&#039;autorisation de faire cette action',
+	'cannot_cont'	   : 'Impossible de continer avec cette requ&ecirc;te',
+	'settings_updated' : 'Param&egrave;tres mis &agrave; jour',
+	
+	// Custom errors:
+	'pp_date_error'     : 'La date doit &ecirc;tre une date valide autoris&eacute;e',
+	'pp_comment_error'  : 'Vous ne pouvez pas poster de commentaire vide',
+	'pp_friend_exist'   : 'Ce membre semble ne pas exister',
+	'pp_friend_already' : 'Vous avez d&eacute;j&agrave; ce membre dans votre liste d&#039;amis',
+	'pp_friend_removed' : 'Ami(s) supprim&eacute;(s)',
+	'pp_icq_error'      : 'Votre identit&eacute; ICQ doit &ecirc;tre num&eacute;rique',
+	
+	// Friend added:
+	'pp_friend_added'     : 'Votre ami a &eacute;t&eacute; ajout&eacute; &agrave; votre liste&nbsp;!',
+	'pp_friend_added_mod' : 'Votre ami doit approuver votre requ&ecirc;te',
+	'pp_friend_approved'  : 'La requ&ecirc;te de votre ami est approuv&eacute;e',
+	
+	// Comments update
+	'pp_comments_updated'  : 'Commentaires mis &agrave; jour',
+	'pp_comment_added_mod' : "Le commentaire doit &ecirc;tre approuv&eacute; avant d&#039;&ecirc;tre publi&eacute;",
+	
+	// POLL MODULE
+	'already_voted' : 'Vous avez d&eacute;j&agrave; vot&eacute;',
+	
+	// ARTICLES MODULE
+	'rating_updated' : 'Votre note a &eacute;t&eacute; mise &agrave; jour',
+	'rating_added'   : 'Votre note a &eacute;t&eacute; ajout&eacute;e',
+	'rating_already' : 'Vous avez d&eacute;j&agrave; not&eacute; cet article'
+};
+
+/**
+* Global stuff
+*/
+
+var ipb_global_lang =
+{
+	// ERROR
+	'post_upload_not_empty'   : 'Vous avez un \u00E9l\u00E9ment \u00E0 uploader. Cliquez sur le bouton "Annuler" puis "Envoyer ce fichier" pour l\'uploader ou cliquez sur OK pour continuer sans l\'uploader.',
+	// EDITORS
+	'editor_enter_url'		  : 'Veuillez saisir l\'URL compl\u00E8te.',
+	'editor_enter_title'      : 'Veuillez saisir le titre de cet \u00E9l\u00E9ment.',
+	'editor_enter_email'      : 'Veuillez saisir l\'adresse de courriel',
+	'editor_enter_image'      : 'Veuillez saisir l\'URL pour cette image',
+	'editor_enter_list'       : 'Veuillez saisir une liste d\'\u00E9l\u00E9ments',
+	// GENERAL
+	'general_error'           : 'Erreur',
+	'general_ok'              : 'OK',
+	'image_resized'           : 'R\u00E9duction \u00E0 <%1>% de la taille originale [ <%2> x <%3> ]',
+	'image_attach_percent'    : 'R\u00E9duite \u00E0 <%1>%',
+	'image_attach_no_percent' : 'Image r\u00E9duite',
+	'image_attach_dims'       : '<%1> x <%2>',
+	'image_attach_size'       : ' (<%1>)',
+	'click_to_view'           : 'Cliquez pour voir l\'image dans sa taille originale',
+	'view_full_size'          : 'Voir l\'image dans sa taille originale',
+	'visit_my_website'		  : 'Visitez mon site web'
+};
